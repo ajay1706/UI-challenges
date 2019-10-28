69c104f443cdab6a110e1d7532bebd03291cca56
ajay1706 <ajay97456@gmail.com>
2019-10-28 08:12:32 +0530

    added IMage card

---
+++ devfest/lib/home/bloc/home_state.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class HomeState extends Equatable {
+  const HomeState();
+}
+
+class InitialHomeState extends HomeState {
+  @override
+  List<Object> get props => [];
+}
