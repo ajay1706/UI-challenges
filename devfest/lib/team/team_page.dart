b40023bb1e1f24d683ce4070f42ee37ccb0850b4
ajay1706 <ajay97456@gmail.com>
2019-10-29 19:30:08 +0530

    teams done

--- devfest/lib/team/team_page.dart
+++ devfest/lib/team/team_page.dart
@@ -5,10 +5,65 @@ import 'package:devfest/home/team_data.dart';
 import 'package:devfest/universal/dev_scaffold.dart';
 import 'package:devfest/utils/tools.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TeamPage extends StatelessWidget {
 
   static const String routeName = '/team_page';
+    _launchUrl(String url)async {
+  if(await canLaunch(url)){
+    await launch(url);
+  }else{
+    throw 'Could not launch $url';
+  }
+}
+  Widget socialActions(context) => FittedBox(
+  child: Row(  
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      IconButton(
+        icon: Icon(FontAwesomeIcons.facebookF),
+        onPressed: () async {
+          await _launchUrl('https://www.facebook.com/profile.php?id=100007109044151');
+        },
+      ),
+       IconButton(
+        icon: Icon(FontAwesomeIcons.twitter),
+        onPressed: () async {
+          await _launchUrl('https://twitter.com/droid_aj');
+        },
+      ),
+       IconButton(
+        icon: Icon(FontAwesomeIcons.linkedin),
+        onPressed: () async {
+          await _launchUrl('https://www.linkedin.com/in/ajay-sharma-33683a10b/');
+        },
+      ),
+        IconButton(
+        icon: Icon(FontAwesomeIcons.instagram),
+        onPressed: () async {
+          await _launchUrl('https://www.instagram.com/droid_aj/');
+        },
+      ),
+        IconButton(
+        icon: Icon(FontAwesomeIcons.github),
+        onPressed: () async {
+          await _launchUrl('https://www.github.com/ajay1706');
+        },
+      ),
+       IconButton(
+        icon: Icon(FontAwesomeIcons.envelope),
+        onPressed: () async {
+          var emailUrl =
+                    '''mailto:ajay97456@gmail.com?subject=Support Needed For DevFest App&body={Name: Ajay Sharma},Email: ajay97456@gmail.com}''';
+                var out = Uri.encodeFull(emailUrl);
+                await _launchUrl(out);
+        },
+      )
+    ],
+  ),
+);
   
   @override
   Widget build(BuildContext context) {
@@ -57,6 +112,8 @@ imageUrl: teams[i].image,
              SizedBox(height: 10,),
             Text(teams[i].contribution,
             style: Theme.of(context).textTheme.caption,),
+            SizedBox(height: 10,),
+            socialActions(context)
 
           ],
         ),
